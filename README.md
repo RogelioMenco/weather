ee1e9605ec34f81ce3a40599f2c2ac1a071698a5
Rogelio Menco <rogeliomenco4@gmail.com>
2024-03-02 14:05:22 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# weather-app
+
+<h3>Weather app developed with React.js</h3>
+<p>A react weather application, developed using React.js and Tailwind CSS</p>
+<p>Try it at:</p>
+<p>
+  <b>Page: </b>https://rogeliomenco.github.io/weather-app
+</p>
---
+++ src/components/AppFooter.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const AppFooter = () => {
+  return (
+    <footer>
+      <div className="p-3 bg-slate-900 bottom-0 mx-auto text-slate-600">
+        <center>
+          <b>
+            Weather App | {""}
+            <a href="https://github.com/RogelioMenco/weather" target="_blank">
+              Github Repository | {""}
+            </a>
+          </b>
+          <p>2024. Rogelio Menco</p>
+        </center>
+      </div>
+    </footer>
+  );
+};
+
+export default AppFooter;
